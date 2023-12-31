73add3b76aabcdc914967dc688ba14be1d507a85
stu <stuart@bitrip.com>
2023-12-31 13:44:17 -0500

    semi-feat: things are centered, but it's ever so slightly off the page

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,16 +1,39 @@
 import { AppBar as MuiAppBar, Toolbar, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
 export const SpotifyAppBar = () => {
   return (
-    <MuiAppBar>
+    <MuiAppBar
+      position="static"
+      sx={{
+        backgroundColor: 'green',
+        border: '1px solid white',
+      }}
+    >
       <Toolbar
         sx={{
           margin: 'auto',
-          width: '80%',
+          width: '60%',
+          border: '2px solid pink',
         }}
       >
-        <Typography variant="h6">Your App Name</Typography>
-        <Typography variant="h6">Profile</Typography>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Your App Name
+        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            flexGrow: 0.25,
+            // flexDirection: 'row',
+            justifyContent: 'space-between',
+            // border: '1px solid blue',
+            // alignItems: 'end',
+          }}
+        >
+          <Typography variant="h6">Profile</Typography>
+          <Typography variant="h6">Stats</Typography>
+          <Typography variant="h6">Logout</Typography>
+        </Box>
       </Toolbar>
     </MuiAppBar>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,35 +1,141 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+// // import '@fontsource/roboto/300.css';
+// // import '@fontsource/roboto/400.css';
+// // import '@fontsource/roboto/500.css';
+// // import '@fontsource/roboto/700.css';
+// // import { useSpotifyStore } from '../store';
+// // import { Container } from '@mui/system';
+// // import { SpotifyAppBar } from './AppBar';
+
+// // type LayoutProps = {
+// //   children: React.ReactNode;
+// // };
+
+// // const Layout: React.FC<LayoutProps> = ({ children }) => {
+// //   const { isLoggedIn, spotifyUser } = useSpotifyStore.getState();
+
+// //   console.log({ isLoggedIn, spotifyUser });
+
+// //   return (
+// //     <>
+// //       <SpotifyAppBar />
+// //       <Container sx={{ width: '100vw', border: '1px solid red' }}>
+// //         <Container
+// //           maxWidth={'xl'}
+// //           sx={{
+// //             margin: 'auto',
+// //             // width: 'inherit',
+// //             display: 'flex',
+// //             justifyContent: 'center',
+// //             alignItems: 'center',
+// //             border: '1px solid limegreen',
+// //           }}
+// //         >
+// //           {children}
+// //         </Container>
+// //       </Container>
+// //     </>
+// //   );
+// // };
+
+// // export default Layout;
+// import React from 'react';
+// import { useSpotifyStore } from '../store';
+// import { Container, ThemeProvider } from '@mui/system';
+// import { SpotifyAppBar } from './AppBar';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import { createTheme } from '@mui/material/styles';
+
+// type LayoutProps = {
+//   children: React.ReactNode;
+// };
+
+// const theme = createTheme();
+
+// const Layout: React.FC<LayoutProps> = ({ children }) => {
+//   const { isLoggedIn, spotifyUser } = useSpotifyStore.getState();
+
+//   console.log({ isLoggedIn, spotifyUser });
+
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <SpotifyAppBar />
+//       <Container sx={{ minHeight: '100vh', border: '1px solid red' }}>
+//         <Container
+//           maxWidth={'xl'}
+//           sx={{
+//             margin: 'auto',
+//             display: 'flex',
+//             flexDirection: 'column',
+//             justifyContent: 'flex-start', // Align to the top
+//             alignItems: 'center',
+//             border: '1px solid limegreen',
+//             paddingTop: theme.spacing(2), // Add some space at the top
+//           }}
+//         >
+//           {children}
+//         </Container>
+//       </Container>
+//     </ThemeProvider>
+//   );
+// };
+
+// export default Layout;
+
+import React from 'react';
 import { useSpotifyStore } from '../store';
-import { Container } from '@mui/system';
+import { Box, Container, ThemeProvider } from '@mui/system';
 import { SpotifyAppBar } from './AppBar';
+import { createTheme } from '@mui/material/styles';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
+const theme = createTheme();
+
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const { isLoggedIn, mode, spotifyUser } = useSpotifyStore.getState();
+  const { isLoggedIn, spotifyUser } = useSpotifyStore.getState();
 
   console.log({ isLoggedIn, spotifyUser });
 
   return (
-    <>
-      <Container sx={{ minWidth: '100vw' }}>
+    <ThemeProvider theme={theme}>
+      <Box
+        display={'flex'}
+        flexDirection={'column'}
+        minWidth={'100vw'}
+        sx={
+          {
+            // overflowX: 'hidden',
+            // margin: 0,
+            // padding: 0,
+          }
+        }
+      >
         <SpotifyAppBar />
         <Container
-          maxWidth={'xl'}
           sx={{
-            margin: 'auto',
-            width: '80%',
+            minHeight: '100vh',
+            border: '1px solid red',
           }}
         >
-          {children}
+          <Container
+            maxWidth={'xl'}
+            sx={{
+              margin: 'auto',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center', // Center horizontally
+              alignItems: 'center', // Center vertically
+              border: '1px solid limegreen',
+              paddingTop: theme.spacing(2), // Add some space at the top
+            }}
+          >
+            {children}
+          </Container>
         </Container>
-      </Container>
-    </>
+      </Box>
+    </ThemeProvider>
   );
 };
 
